e9566b920d19b76ff112f3608520d57201c4fd91
annasitsiliya <annasitsiliya@gmail.com>
2022-08-20 18:55:23 +0200

    Refactoring SearchFieldTest

--- src/test/java/org/example/automation/amazon/SearchFieldTest.java
+++ src/test/java/org/example/automation/amazon/SearchFieldTest.java
@@ -5,16 +5,14 @@ import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class SearchFieldTest extends BaseWebDriverTest {
-
     public static final String INCORRECT_INPUT = "890770hhbhb767879zdrjh444:::";
-
     public static final String PRESENT = "ipad";
-
     public static final String TEXT = "iPad";
 
     @FindBy(css = ".nav-search-field input")
@@ -23,7 +21,6 @@ public class SearchFieldTest extends BaseWebDriverTest {
     @FindBy(xpath = "//input[@id=\"nav-search-submit-button\"]")
     private WebElement searchButton;
 
-    //890770hhbhb767879zdrjh444:::
     @FindBy(xpath = "//div[@tabindex = 0]/div[@class='a-row']")
     private WebElement searchResult;
 
@@ -33,18 +30,15 @@ public class SearchFieldTest extends BaseWebDriverTest {
     @FindAll({@FindBy(xpath = "//a/span[@class ='a-size-medium a-color-base a-text-normal']")})
     public List<WebElement> searchResultList;
 
-    // @FindBy(xpath = "//a/span[@class ='a-size-medium a-color-base a-text-normal']")
-    //  private WebElement[] searchResultText;
-
-
+    @BeforeMethod
+    public void setup() {
+        PageFactory.initElements(webDriver, this);
+        webDriver.get("https://www.amazon.com");
+    }
     @Test
     public void noResultsForIncorrectInformationTest() {
 
-        PageFactory.initElements(webDriver, this);
-        webDriver.get("https://www.amazon.com");
-
         searchField.sendKeys(INCORRECT_INPUT);
-
 
         searchButton.click();
 
@@ -55,15 +49,10 @@ public class SearchFieldTest extends BaseWebDriverTest {
         Assert.assertEquals(actualResult, expectedResult);
 
     }
-
     @Test
     public void checkResultsForPresentTest() {
 
-        PageFactory.initElements(webDriver, this);
-        webDriver.get("https://www.amazon.com");
-
         searchField.sendKeys(PRESENT);
-
 
         searchButton.click();
 
@@ -75,9 +64,6 @@ public class SearchFieldTest extends BaseWebDriverTest {
     }
     @Test
     public void checkResultsForTextTest() {
-
-        PageFactory.initElements(webDriver, this);
-        webDriver.get("https://www.amazon.com");
 
         searchField.sendKeys(PRESENT);
 
