1b023826019d273459b9ebed9978f1b221d1c205
NadiiaPerevertova <nadya.perevertova@gmail.com>
2022-08-22 10:45:44 +0200

    extract waiting timeOut as a constant

--- src/test/java/org/example/automation/amazon/DeliverToTest.java
+++ src/test/java/org/example/automation/amazon/DeliverToTest.java
@@ -12,6 +12,7 @@ import java.time.Duration;
 public class DeliverToTest extends BaseWebDriverTest {
 
     public static final String AMAZON_COM = "https://www.amazon.com";
+    public static final Duration timeOut= Duration.ofSeconds(20);
 
     @Test(description = "The test checks shipping changes by US state zip codes.")
     public void verifyDeliveryLocationChanging() {
@@ -20,18 +21,18 @@ public class DeliverToTest extends BaseWebDriverTest {
         WebElement deliverIcon = webDriver.findElement(By.id("nav-global-location-data-modal-action"));
         deliverIcon.click();
 
-        WebElement fieldUsZipCode = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement fieldUsZipCode = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXZipUpdateInput")));
         fieldUsZipCode.sendKeys("95814");
 
         WebElement buttonApply = webDriver.findElement(By.id("GLUXZipUpdate"));
         buttonApply.click();
 
-        WebElement buttonContinue = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement buttonContinue = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.a-popover-footer #GLUXConfirmClose")));
         buttonContinue.click();
 
-        boolean isTrueDeliver = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        boolean isTrueDeliver = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.textToBe(By.id("glow-ingress-line2"), "Sacramento 95814\u200C"));
 
         Assert.assertTrue(isTrueDeliver, "Not updated, expected: Sacramento 95814");
@@ -44,11 +45,11 @@ public class DeliverToTest extends BaseWebDriverTest {
         WebElement deliverIcon = webDriver.findElement(By.id("nav-global-location-data-modal-action"));
         deliverIcon.click();
 
-        WebElement countryDropDown = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement countryDropDown = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryListDropdown")));
         countryDropDown.click();
 
-        boolean isPolandPresent = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        boolean isPolandPresent = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.textToBe(By.id("GLUXCountryList_178"), "Poland"));
         Assert.assertTrue(isPolandPresent, "Not found Poland");
     }
@@ -60,30 +61,30 @@ public class DeliverToTest extends BaseWebDriverTest {
         WebElement deliverIcon = webDriver.findElement(By.id("nav-global-location-data-modal-action"));
         deliverIcon.click();
 
-        WebElement countryDropDown = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement countryDropDown = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryListDropdown")));
         countryDropDown.click();
 
-        WebElement chooseAustralia = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement chooseAustralia = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXCountryList_0")));
         chooseAustralia.click();
 
         WebElement buttonDone = webDriver.findElement(By.name("glowDoneButton"));
         buttonDone.click();
 
-        boolean isTrueDeliver = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        boolean isTrueDeliver = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.textToBe(By.id("glow-ingress-line2"), "Australia"));
         Assert.assertTrue(isTrueDeliver, "Not updated, expected: Australia");
 
-        WebElement chooseChairs = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement chooseChairs = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@alt=\"Chairs\"]")));
         chooseChairs.click();
 
-        WebElement categoryResuls = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        WebElement categoryResuls = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-component-type=\"s-search-results\"]//img[1]")));
         categoryResuls.click();
 
-        boolean isAustraliaDeliveryPresent = new WebDriverWait(webDriver, Duration.ofSeconds(20))
+        boolean isAustraliaDeliveryPresent = new WebDriverWait(webDriver, timeOut)
                 .until(ExpectedConditions.textToBe(By.id("contextualIngressPtLabel_deliveryShortLine"), "Deliver to Australia"));
         Assert.assertTrue(isAustraliaDeliveryPresent, "Not found 'Deliver to Australia'");
     }
