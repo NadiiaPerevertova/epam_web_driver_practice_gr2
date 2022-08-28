28918e36add525bd80c239cf3111cbe3c594e9cc
NadiiaPerevertova <nadya.perevertova@gmail.com>
2022-08-28 21:11:09 +0200

    implement first Test

--- src/test/java/org/example/automation/amazon/pageobject/DeliverToTest.java
+++ src/test/java/org/example/automation/amazon/pageobject/DeliverToTest.java
@@ -1,13 +1,21 @@
 package org.example.automation.amazon.pageobject;
 
 import org.example.automation.amazon.pageobject.pages.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DeliverToTest extends BaseTest {
 
-    @Test
+    @Test(description = "The test checks shipping changes by US state zip codes.")
     public void verifyDeliveryLocationChanging() {
         HomePage home = new HomePage();
-        home.open();
+        String deliveryToPlace = home
+                .open()
+                .changeDeliverLocation()
+                .enterZipCode("95814")
+                .apply()
+                .confirmApply()
+                .getDeliverLocation("Sacramento 95814\u200C");
+        Assert.assertEquals(deliveryToPlace, "Sacramento 95814\u200C");
     }
 }
