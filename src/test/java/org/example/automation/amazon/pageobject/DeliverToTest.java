a498f5cf1de0e7b2e534656912aa230df353f951
NadiiaPerevertova <nadya.perevertova@gmail.com>
2022-08-29 22:28:30 +0200

    Implement deliver to tests

--- src/test/java/org/example/automation/amazon/pageobject/DeliverToTest.java
+++ src/test/java/org/example/automation/amazon/pageobject/DeliverToTest.java
@@ -18,4 +18,34 @@ public class DeliverToTest extends BaseTest {
                 .getDeliverLocation("Sacramento 95814\u200C");
         Assert.assertEquals(deliveryToPlace, "Sacramento 95814\u200C");
     }
+
+    @Test(description = "The test checks that in the list of countries Poland is present")
+    public void verifyCountryPresent(){
+        HomePage home = new HomePage();
+        String presentCountry = home
+                .open()
+                .changeDeliverLocation()
+                .countryDropDown()
+                .getDeliverCountry("Poland");
+        Assert.assertEquals(presentCountry, "Poland");
+    }
+
+    @Test(description = "The test checks that changed delivery location is reflected in selected product.")
+    public void verifyDeliveryOfSelectedProduct(){
+        HomePage home = new HomePage();
+        String deliveryPlace = home
+                .open()
+                .changeDeliverLocation()
+                .countryDropDown()
+                .selectDeliverCountry("Australia")
+                .done()
+                .getDeliverLocation("Australia");
+        Assert.assertEquals(deliveryPlace, "Australia");
+
+        String itemDeliveryInfo = home
+                .chooseCategory("Chairs")
+                .chooseFirstResult()
+                .getItemDeliveryLocation();
+        Assert.assertEquals(itemDeliveryInfo,"Deliver to Australia");
+    }
 }
